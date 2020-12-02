35e67b1d41a984694f414e11745c26374011f473
Thomas M. DuBuisson <tommd@muse.dev>
2020-12-02 08:41:54 -0800

    Day2 part 2 hack

--- Day2/Day2.hs
+++ Day2/Day2.hs
@@ -3,6 +3,8 @@
     build-depends: base, containers
 -}
 
+import Data.Maybe
+
 data Policy = Policy
     { letter :: Char
     , low :: Int
@@ -28,6 +30,13 @@ isValid (Entry pol pass) =
     let len = length (filter (== letter pol) pass)
     in len <= high pol && len >= low pol
 
+isValid_part2 :: Entry -> Bool
+isValid_part2 (Entry pol pass) =
+    let indexpass = zip [1..] pass
+        chars = catMaybes [lookup (low pol) indexpass, lookup (high pol) indexpass]
+        len = length (filter ((== letter pol)) chars)
+    in len == 1
+
 main = do
     entries <- readEntries "input"
-    print $ length (filter isValid entries)
+    print $ length (filter isValid_part2 entries)
